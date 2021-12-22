7d0903f2090f00b8647a6cedeac4abc34d257094
abraaop <abraomportela@fac.pe.senac.br>
2021-12-22 16:34:44 -0300

    Fluxo caso a pessoa não queira saber do IMC

--- imc/src/imc/Imc.java
+++ imc/src/imc/Imc.java
@@ -6,30 +6,36 @@ public class Imc {
 
 	public static void main(String[] args) {
 		int resposta = JOptionPane.showConfirmDialog(null, "Olá, vamos descobrir seu IMC?");
+		if(resposta==0) {
+			
+			String altura = JOptionPane.showInputDialog("Qual a sua altura?");
+			String peso = JOptionPane.showInputDialog("Qual o seu peso?");
 
-		String altura = JOptionPane.showInputDialog("Qual a sua altura?");
-		String peso = JOptionPane.showInputDialog("Qual o seu peso?");
+			double dAltura = Double.parseDouble(altura);
+			double dPeso = Double.parseDouble(peso);
 
-		double dAltura = Double.parseDouble(altura);
-		double dPeso = Double.parseDouble(peso);
+			double imc = (dPeso / Math.pow(dAltura, 2));
 
-		double imc = (dPeso / Math.pow(dAltura, 2));
+			if (imc < 18.5) {
+				JOptionPane.showMessageDialog(null, "Você está abaixo do peso, seu IMC é " + imc);
+			} else if (imc >= 18.5 && imc < 25) {
+				JOptionPane.showMessageDialog(null, "Você está com peso ideal, seu IMC é " + imc);
+			} else if (imc >= 25 && imc < 30) {
+				JOptionPane.showMessageDialog(null, "Você está levemente acima do peso, seu IMC é " + imc);
+			} else if (imc >= 30 && imc < 35) {
+				JOptionPane.showMessageDialog(null, "Você está com obesidade de grau I, seu IMC é " + imc);
+			} else if (imc >= 35 && imc < 40) {
+				JOptionPane.showMessageDialog(null, "Você está com obesidade de grau II, seu IMC é " + imc);
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"Obesidade III, seu IMC é " + imc + ". Por favor, procure um nutricionista.");
 
-		if (imc < 18.5) {
-			JOptionPane.showMessageDialog(null, "Você está abaixo do peso, seu IMC é " + imc);
-		} else if (imc >= 18.5 && imc < 25) {
-			JOptionPane.showMessageDialog(null, "Você está com peso ideal, seu IMC é " + imc);
-		} else if (imc >= 25 && imc < 30) {
-			JOptionPane.showMessageDialog(null, "Você está levemente acima do peso, seu IMC é " + imc);
-		} else if (imc >= 30 && imc < 35) {
-			JOptionPane.showMessageDialog(null, "Você está com obesidade de grau I, seu IMC é " + imc);
-		} else if (imc >= 35 && imc < 40) {
-			JOptionPane.showMessageDialog(null, "Você está com obesidade de grau II, seu IMC é " + imc);
+			}
 		} else {
-			JOptionPane.showMessageDialog(null,
-					"Obesidade III, seu IMC é " + imc + ". Por favor, procure um nutricionista.");
-
+			JOptionPane.showMessageDialog(null, "Ok, quando quiser saber do seu IMC, por favor, volte!");
 		}
+
+		
 
 	}
 }
