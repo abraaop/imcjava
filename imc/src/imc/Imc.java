55171837adc30b93ee1b674f5ae1edc5881481d3
abraaop <abraomportela@fac.pe.senac.br>
2021-12-27 15:13:14 -0300

    Decimal arredondado com 2 casas decimais.

--- imc/src/imc/Imc.java
+++ imc/src/imc/Imc.java
@@ -1,5 +1,7 @@
 package imc;
 
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
 public class Imc {
@@ -15,20 +17,22 @@ public class Imc {
 			double dPeso = Double.parseDouble(peso);
 
 			double imc = (dPeso / Math.pow(dAltura, 2));
+	        System.out.println( imc );
+	        DecimalFormat df=new DecimalFormat("##.##");
+	        JOptionPane.showMessageDialog(null, "IMC:"+df.format(imc));
 
 			if (imc < 18.5) {
-				JOptionPane.showMessageDialog(null, "Você está abaixo do peso, seu IMC é " + imc);
+				JOptionPane.showMessageDialog(null, "Você está abaixo do peso, seu IMC é " +df.format(imc));
 			} else if (imc >= 18.5 && imc < 25) {
-				JOptionPane.showMessageDialog(null, "Você está com peso ideal, seu IMC é " + imc);
+				JOptionPane.showMessageDialog(null, "Você está com peso ideal, seu IMC é " +df.format(imc));
 			} else if (imc >= 25 && imc < 30) {
-				JOptionPane.showMessageDialog(null, "Você está levemente acima do peso, seu IMC é " + imc);
+				JOptionPane.showMessageDialog(null, "Você está levemente acima do peso, seu IMC é " +df.format(imc));
 			} else if (imc >= 30 && imc < 35) {
-				JOptionPane.showMessageDialog(null, "Você está com obesidade de grau I, seu IMC é " + imc);
+				JOptionPane.showMessageDialog(null, "Você está com obesidade de grau I, seu IMC é " +df.format(imc));
 			} else if (imc >= 35 && imc < 40) {
-				JOptionPane.showMessageDialog(null, "Você está com obesidade de grau II, seu IMC é " + imc);
+				JOptionPane.showMessageDialog(null, "Você está com obesidade de grau II, seu IMC é " +df.format(imc));
 			} else {
-				JOptionPane.showMessageDialog(null,
-						"Obesidade III, seu IMC é " + imc + ". Por favor, procure um nutricionista.");
+				JOptionPane.showMessageDialog(null, "Obesidade III, seu IMC é " + imc + ". Por favor, procure um nutricionista.");
 
 			}
 		} else {
